4a8035387635af5b1d64fc281caa8dc8afb5e775
Anas-aldowlah <153012667+Anas-aldowlah@users.noreply.github.com>
2026-07-01 00:17:47 +0300

    2nd commit

--- obj/Debug/net9.0/YAGOT_2.0.AssemblyInfo.cs
+++ obj/Debug/net9.0/YAGOT_2.0.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("YAGOT_2.0")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5636903b7e0aa7dd6e08dab3e175b3b02e0ee019")]
 [assembly: System.Reflection.AssemblyProductAttribute("YAGOT_2.0")]
 [assembly: System.Reflection.AssemblyTitleAttribute("YAGOT_2.0")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
