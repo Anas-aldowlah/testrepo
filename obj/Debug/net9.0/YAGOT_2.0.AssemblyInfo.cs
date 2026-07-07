f9d0e965db1806bf0f8e99630a1d056ecde9f09e
Anas-aldowlah <153012667+Anas-aldowlah@users.noreply.github.com>
2026-07-07 14:02:18 +0300

    Modified/New Files
    Views/Products/Details.cshtml (redesigned)
    wwwroot/css/products/details.css (new)
    wwwroot/js/products/details.js (new)

--- obj/Debug/net9.0/YAGOT_2.0.AssemblyInfo.cs
+++ obj/Debug/net9.0/YAGOT_2.0.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("YAGOT_2.0")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+09049ce7cff675f6bfc5c8d43c32658f1e4df0f8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5fb38c5ed34563d287875d06adcf73beefe68d5e")]
 [assembly: System.Reflection.AssemblyProductAttribute("YAGOT_2.0")]
 [assembly: System.Reflection.AssemblyTitleAttribute("YAGOT_2.0")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
