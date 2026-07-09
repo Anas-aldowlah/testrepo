a3ca21c2907039bc9cc81b7eb67ffb18d218113b
assil <assilmhmdalmnssoty@gmail.com>
2026-07-09 17:55:24 +0300

    التعديل على صفحة تسجيل الدخول وعلى منطق التسجيل واضافة الايميل وتشفير ارقام الجوالات

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -63,5 +63,6 @@ public class HomeController : Controller
     public IActionResult Error()
     {
         return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+
     }
 }
